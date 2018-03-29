b96378bb673895f525a2e886ff73e6be946cb34d
Yota Toyama <raviqqe@gmail.com>
2018-03-29 16:32:22 +0900

    Fix pinned boxed async functions

--- futures/tests/async_await/pinned.rs
+++ futures/tests/async_await/pinned.rs
@@ -1,4 +1,5 @@
 use futures::stable::block_on_stable;
+use futures::executor::{block_on, ThreadPool};
 use futures::prelude::*;
 
 #[async]
@@ -14,6 +15,21 @@ fn bar(x: &i32) -> Result<i32, i32> {
 #[async]
 fn baz(x: i32) -> Result<i32, i32> {
     await!(bar(&x))
+}
+
+#[async(pinned)]
+fn boxed(x: i32) -> Result<i32, i32> {
+    Ok(x)
+}
+
+#[async(pinned_send)]
+fn boxed_send(x: i32) -> Result<i32, i32> {
+    Ok(x)
+}
+
+#[async(pinned_send)]
+fn spawnable() -> Result<(), Never> {
+    Ok(())
 }
 
 #[async_stream(item = u64)]
@@ -40,5 +56,13 @@ fn main() {
     assert_eq!(block_on_stable(foo()), Ok(1));
     assert_eq!(block_on_stable(bar(&1)), Ok(1));
     assert_eq!(block_on_stable(baz(17)), Ok(17));
+    assert_eq!(block_on(boxed(17)), Ok(17));
+    assert_eq!(block_on(boxed_send(17)), Ok(17));
     assert_eq!(block_on_stable(uses_async_for()), Ok(vec![0, 1]));
+}
+
+#[test]
+fn run_pinned_future_in_thread_pool() {
+    let mut pool = ThreadPool::new();
+    pool.spawn_pinned(spawnable()).unwrap();
 }
